0ccc8f5a572c681f0882405a46ed86839a3e23fa
Cody Schwengler01~ <cschwengler@outlook.com>
2023-08-20 12:52:07 -0700

    created loop for array and compared it to time.

--- Assets/script.js
+++ Assets/script.js
@@ -23,9 +23,24 @@ let hoursWorkDay = [
 
 $(document).ready(function () {
   //Added code to display the current date in the header of the page.
-  var today = dayjs();
-  $("#currentDay").text(today.format("dddd, MMMM DD HH:mmA"));
-
-  console.log(hoursWorkDay[0].text());
-
+  let time = dayjs();
+  $("#currentDay").text(time.format("dddd, MMMM DD HH:mmA"));
+  //We start by having the array hoursWordDay put into a loop.
+  $(hoursWorkDay).each(function (index, hourElement) {
+    // create variable current hour from the dayjs function and get the hour only//
+    //we then create a variable called blockHour to get the id number of the time-block//
+    let currentHour = dayjs().hour();
+    let blockHour = parseInt(hourElement.attr("id").split("-")[1]);
+    //then we put logic to put the apopriate css style in the hour element by remvoing and
+    //placing a new class in it.
+    if (currentHour === blockHour) {
+      hourElement.removeClass("future");
+      hourElement.addClass("present");
+    } else if (currentHour > blockHour) {
+      hourElement.removeClass("future");
+      hourElement.addClass("past");
+    } else {
+      hourElement.addClass("future");
+    }
+  });
 });
